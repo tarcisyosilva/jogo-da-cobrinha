c9bc69241a8524da36b2796e973b66f81297981f
Tarcisyo Ferreira Da Silva <tarcisyo01@gmail.com>
2020-12-10 17:03:20 -0300

    Criar cobrinha

--- app.js
+++ app.js
@@ -1,10 +1,26 @@
 let canvas = document.getElementById("snake"),
     context = canvas.getContext("2d"),
-    box = 32;
+    box = 32,
+    snake = [];
+
+    snake[0] = {
+        x: 8 * box,
+        y: 8 * box
+    }
 
     function criarBG() {
         context.fillStyle = "lightgreen";
-        context.fillRect(0,0,16 * box, 16 * box);
+        context.fillRect(0, 0, 16 * box, 16 * box);
     }
 
+    function criarCobrinha(){
+        for(i=0; i < snake.length; i++){
+            context.fillStyle = "green";
+            context.fillRect(snake[i].x, snake[i].y, box, box);
+        }
+    }
+
+
     criarBG();
+    criarCobrinha();
+
